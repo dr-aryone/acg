44d4f062c611e514e2334d94813a4c48effc0266
Mak12776 <mak12776@gmail.com>
2019-03-01 14:20:30 +0330

    we just need generate code, nothing else!!!!

--- acg/core.py
+++ acg/core.py
@@ -6,6 +6,7 @@ try:
 except ImportError:
     from _dummy_thread import allocate_lock
 
+
 class BytesStream:
     def __init__(self, raw: io.RawIOBase, buffer_size):
         if buffer_size <= 0:
@@ -16,31 +17,35 @@ class BytesStream:
 
     def __getitem__(self, item):
         if isinstance(item, int):
-            while item <= len(self._buffer):
+            while item > len(self._buffer):
                 new_buffer = self._raw.read(self._buffer_size)
                 if not new_buffer:
                     raise IndexError(item)
                 self._buffer.extend(new_buffer)
             return self._buffer[item]
         if isinstance(item, slice):
-            while item.stop <= len(self._buffer)
+            while item.stop > len(self._buffer):
                 new_buffer = self._raw.read(self._buffer_size)
                 if not new_buffer:
-                    break
-
+                    raise IndexError(item)
+                self._buffer.extend(new_buffer)
+            return self._buffer[item]
         raise TypeError(item.__class__.__name__)
 
     def __len__(self):
         return len(self._buffer)
 
     def remove(self, size):
+        if size <= 0:
+            raise ValueError('invalid size bytes')
+        self._buffer = self._buffer[size + 1:]
 
+
+class CodeSyntaxError(Exception):
+    pass
 
 
 class BaseNode:
-    def write(self, outfile: io.BufferedWriter, options):
-        pass
-
     @staticmethod
-    def read(self, infile: io.BufferedReader, options):
+    def read_from(self, buffer: BytesStream, options):
         pass
--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import acg
 
 def main():
     pass
